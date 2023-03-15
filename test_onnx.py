4a9ecf09f590d34c516f74b26fa761294f119c6b
HishamElamir <shadow_123413@yahoo.com>
2023-03-15 13:29:52 +0200

    documenting test_onnx file

--- test_onnx.py
+++ test_onnx.py
@@ -1,13 +1,16 @@
+# IMPORT
 from PIL import Image
 from torch import argmax
 import numpy as np
 import torchvision.transforms as transforms
 import onnxruntime as onnxrt
 
+# TRANSFORM INPUT/OUTPUT TO NUMPY
 def to_numpy(tensor):
     return tensor.detach().cpu().numpy()\
         if tensor.requires_grad else tensor.cpu().numpy()
 
+# TRANSFORMING IMAGE TO BE VALID FOR MODEL
 def preprocess_numpy(img):
     resize = transforms.Resize((224, 224))   #must same as here
     crop = transforms.CenterCrop((224, 224))
@@ -20,13 +23,16 @@ def preprocess_numpy(img):
     img.unsqueeze_(0)
     return img
 
+# LOADING ONNX FILE FOR INFERENCE 
 onnx_session= onnxrt.InferenceSession("mtailor-resnet.onnx")
 
+# GETTING THE 2 TEST IMAGES
 img1 = Image.open("mtailor_mlops_assessment/n01440764_tench.jpeg")
 img1 = preprocess_numpy(img1)
 img2 = Image.open("mtailor_mlops_assessment/n01667114_mud_turtle.JPEG")
 img2 = preprocess_numpy(img2)
 
+# RUNNING AND ASSERTING ON FIRST IMAGE
 onnx_inputs= {
     onnx_session.get_inputs()[0].name: to_numpy(img1)
 }
@@ -34,6 +40,7 @@ onnx_output = onnx_session.run(None, onnx_inputs)
 img_label = onnx_output[0]
 assert np.argmax(img_label) == 0
 
+# RUNNING AND ASSERTING ON SECOND IMAGE
 onnx_inputs= {
     onnx_session.get_inputs()[0].name: to_numpy(img2)
 }
